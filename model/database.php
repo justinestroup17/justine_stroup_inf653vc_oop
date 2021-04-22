6265607c05620c2b059feae51b767381a5cc384e
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-04-22 15:12:14 -0600

    Changed database.php to oop

--- model/database.php
+++ model/database.php
@@ -1,27 +1,35 @@
 <?php
-    //local development server connection
-    /*
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';*/
-    //$password = 'pa55word';
-
-    // Heroku connection
-    $dsn = 'mysql:host=z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=q5eb1vj9faharoe4';
-    $username = 'qt34l1n36ohrcfn6';
-    $password = 'rbqy5g1b3879gv4e';
+class Database {
+    // Heroku connection:
+    private static $dsn = 'mysql:host=z5zm8hebixwywy9d.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=q5eb1vj9faharoe4';
+    private static $username = 'qt34l1n36ohrcfn6';
+    private static $password = 'rbqy5g1b3879gv4e';
+    private static $db;
     
-    try {
-        //local development server connection
-        //if using a $password, add it as 3rd parameter
-        //$db = new PDO($dsn, $username);
+    // (local development server connection):
+    // private static $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
+    // private static $username = 'root';
+    // private static $password = 'pa55word';
 
-        // Heroku connection
-        $db = new PDO($dsn, $username, $password);
-    } catch (PDOException $e) {
-        $error = "Database Error: ";
-        $error .= $e->getMessage();
-        //include('../view/error.php');
-        include('C:\xampp\htdocs\justine_stroup_inf653vc_authentication\view\error.php');
-        exit();
+    private function __construct() {}
+
+    public static function getDB() {
+        if (!isset(self::$db)) {
+            try {
+                // Heroku connection:
+                self::$db = new PDO(self::$dsn, self::$username, self::$password);
+
+                // (local development server connection):
+                // if using a $password, add it as 3rd parameter
+                // $db = new PDO($dsn, $username);
+            } catch (PDOException $e) {
+                $error = "Database Error: ";
+                $error .= $e->getMessage();
+                include('../view/error.php');
+                exit();
+            }
+        }
+        return self::$db;
     }
+}
 ?>
