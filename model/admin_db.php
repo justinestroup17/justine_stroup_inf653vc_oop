cef31ced106c958c15d27127a42bafe326ddabda
Justine Stroup <jjstroup@mail.fhsu.edu>
2021-04-22 16:51:12 -0600

    Modified all model files to oop

--- model/admin_db.php
+++ model/admin_db.php
@@ -1,40 +1,42 @@
 <?php
-function add_admin($username, $password) {
-    global $db;
-    $hash = password_hash($password, PASSWORD_DEFAULT);
-    $query = 'INSERT INTO administrators
-                (username, password)
-              VALUES
-                (:username, :password)';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':username', $username);
-    $statement->bindValue(':password', $hash);
-    $statement->execute();
-    $statement->closeCursor();
-}
-function is_valid_admin_login($username, $password) {
-    global $db;
-    $query = 'SELECT password
-              FROM administrators
-              WHERE username = :username';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':username', $username);
-    $statement->execute();
-    $row = $statement->fetch();
-    $statement->closeCursor();
-    $hash = (!empty($row['password'])) ? $row['password'] : NULL;
-    return password_verify($password, $hash);
-}
-function username_exists($username) {
-    global $db;
-    $query = 'SELECT COUNT(username)
-              FROM administrators
-              WHERE username = :username';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':username', $username);
-    $statement->execute();
-    $result = $statement->fetchColumn();
-    $statement->closeCursor();
-    return $result;
+Class AdminDB {
+    public static function add_admin($username, $password) {
+        $db = Database::getDB();
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $query = 'INSERT INTO administrators
+                    (username, password)
+                VALUES
+                    (:username, :password)';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':username', $username);
+        $statement->bindValue(':password', $hash);
+        $statement->execute();
+        $statement->closeCursor();
+    }
+    public static function is_valid_admin_login($username, $password) {
+        $db = Database::getDB();
+        $query = 'SELECT password
+                FROM administrators
+                WHERE username = :username';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':username', $username);
+        $statement->execute();
+        $row = $statement->fetch();
+        $statement->closeCursor();
+        $hash = (!empty($row['password'])) ? $row['password'] : NULL;
+        return password_verify($password, $hash);
+    }
+    public static function username_exists($username) {
+        $db = Database::getDB();
+        $query = 'SELECT COUNT(username)
+                FROM administrators
+                WHERE username = :username';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':username', $username);
+        $statement->execute();
+        $result = $statement->fetchColumn();
+        $statement->closeCursor();
+        return $result;
+    }
 }
 ?>
